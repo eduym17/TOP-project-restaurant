e960eedbf5780ec41c6a2a997a146c18239d3dd3
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-01 16:27:52 -0600

    chore: add main route

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // import FUNCTION from SRC
 
 // AQUÍ se llama a las funciones de despliegue para las distintas views, llamando primero a la función de despliegue principal
+
+import loadHome from './views/home'
+
+loadHome();

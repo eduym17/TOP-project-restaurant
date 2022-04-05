39a6b5111bba5c5a670f0bc04b93115ebbbb6c3f
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-04 20:29:21 -0500

    chore: load landing page and config sources

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // import FUNCTION from SRC
 
 // AQUÍ se llama a las funciones de despliegue para las distintas views, llamando primero a la función de despliegue principal
+import loadInitialPage from './views/initialPage'
 
-import loadHome from './views/home'
+start();
 
-loadHome();
+function start() {
+  loadInitialPage();
+}

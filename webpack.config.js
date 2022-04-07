d2965d46bd805b525e30a55f95889d1b21f8efbb
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-07 16:11:15 -0500

    Page deployment

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
-  output: {
-    publicPath: "/TOP-project-restaurant/",
-  },
+  module: {
+    rules: [{
+      test: [/\.css$/i, /\.html$/i],
+      use: ['style-loader', 'css-loader', 'html-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    }]
+  }
 };
